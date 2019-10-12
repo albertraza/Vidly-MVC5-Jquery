d42f3cf8f7ded5c6c05f2a71f3f87eda6f9de8fd
Albertraza <arbert.jr@gmail.com>
2019-10-11 21:20:43 -0400

    Added Movie view and new ByReleasedDate Controller

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "Movies/released/{month}/{year}",
+                new { controller = "Movie", action = "ByReleaseDate" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
