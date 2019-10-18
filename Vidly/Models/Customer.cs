f812b61db814a4b241b3795bcfc94c642afd8dec
Albertraza <arbert.jr@gmail.com>
2019-10-18 16:58:39 -0400

    Added IsSubscribedToNewsletter prop in class and in DB

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,6 +9,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSuscribedToNewsletter { get; set; }
 
         public List<Customer> GetCustomer()
         {
