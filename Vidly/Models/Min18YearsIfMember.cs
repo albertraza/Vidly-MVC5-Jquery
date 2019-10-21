889caebf0dce1ccce748819a07922bb3401116ad
Albertraza <arbert.jr@gmail.com>
2019-10-21 16:15:17 -0400

    added Birthdate server-side validation

---
+++ Vidly/Models/Min18YearsIfMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == MembershipType.Unknow ||
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
+                return ValidationResult.Success;
+
+            if (!customer.BirthDate.HasValue)
+                return new ValidationResult("Birthdate is required");
+
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            return (age >= 18) ? ValidationResult.Success
+                : new ValidationResult("The user needs to be 18 years old to have a membership");
+        }
+    }
+}
