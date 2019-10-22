7cefae8d34288afbc33cedb9f737941742f51c53
Albertraza <arbert.jr@gmail.com>
2019-10-21 21:44:23 -0400

    set up th Restful API and Mapper

---
+++ Vidly/DTOs/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Min18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSuscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
